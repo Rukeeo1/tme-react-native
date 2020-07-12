747601b49e134e96e0f2dc81ee7b525f742977c1
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-07-12 12:52:29 +0100

    ft: create custom input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, TextInput, StyleSheet, Keyboard } from 'react-native';
 
-export default function Input() {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+type Props = {
+  placeholder: string;
+  handleChange: (e: string) => void;
+  multiline?: boolean;
+  returnKeyType?: string;
+};
+
+export default function Input({
+  placeholder,
+  handleChange,
+  multiline,
+  returnKeyType,
+}: Props) {
+  return (
+    <View style={styles.input}>
+      <TextInput
+        placeholder={placeholder}
+        onChangeText={handleChange}
+        multiline={multiline}
+        onBlur={() => Keyboard.dismiss()}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  input: {
+    borderBottomColor: 'grey',
+    borderBottomWidth: 0.5,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+});
