52969937472584cb62815b3f12aa96a9bf45ff1b
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-07-12 07:27:48 +0100

    chore: add the react navigation and necessary app routes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './screens/Login';
+import SignUp from './screens/SignUp';
+import Todos from './screens/Todos';
+import TodoDetails from './screens/TodoDetails'
+
+//mapping for routes...
+export type RootStackParamList = {
+  Login: undefined;
+  SignUp: undefined;
+  Todos: undefined;
+  TodoDetails: undefined;
+};
+
+// create stack navigator
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const { Navigator, Screen } = Stack;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator>
+        <Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Screen
+          name="Todos"
+          component={Todos}
+          options={{
+            title: '',
+            headerTintColor: '#fff',
+            headerLeft: () => (
+              <View style={styles.headerTextContainer}>
+                <Text style={styles.headerText}>Rukee Ojigbo</Text>
+              </View>
+            ),
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Screen
+          name="TodoDetails"
+          component={TodoDetails}
+
+        />
+      </Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,5 +66,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headerTextContainer: {
+    paddingLeft: 10,
+  },
+  headerText: {
+    // color: 'white',
+    fontWeight: 'bold',
+    fontSize: 17,
   },
 });
