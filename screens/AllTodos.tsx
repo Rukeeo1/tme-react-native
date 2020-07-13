65925ed4ddb74b1d193b2c03e044e28f364f3d26
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-07-13 15:25:37 +0100

    chore: add empty state

--- screens/AllTodos.tsx
+++ screens/AllTodos.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
+import { StyleSheet, View, ScrollView, ActivityIndicator } from 'react-native';
 //components
 import TodoItem from '../components/TodoItem';
 import Navigation from '../components/Navigation';
@@ -7,7 +7,7 @@ import Navigation from '../components/Navigation';
 //redux
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchTodos } from '../store/actions/todos';
-import Todos from './Todos';
+import Empyty from '../components/EmtpyState';
 
 interface ITodo {
   title: string;
@@ -21,33 +21,45 @@ export default function AllTodos() {
   useEffect(() => {
     dispatch(fetchTodos());
   }, []);
-  const { todos } = useSelector((state) => state?.todos);
+  const { todos, loading } = useSelector((state) => state);
   // sort by time
-  const sorted = todos.sort((a: object, b: object) => {
+  const sorted = todos.todos.sort((a: object, b: object) => {
     const aDates = new Date(a.dueDate).getTime();
     const bDates = new Date(b.dueDate).getTime();
     return aDates < bDates ? 1 : -1;
   });
 
   return (
-    <View style={{ flex: 1 }}>
-      <ScrollView contentContainerStyle={styles.container}>
-        {sorted?.map((todo: ITodo, index: number) => (
-          <React.Fragment key={index}>
-            <TodoItem todo={todo} />
-          </React.Fragment>
-        ))}
-      </ScrollView>
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <>
+        <ScrollView contentContainerStyle={styles.container}>
+          {sorted?.map((todo: ITodo, index: number) => (
+            <React.Fragment key={index}>
+              <TodoItem todo={todo} />
+            </React.Fragment>
+          ))}
+          <View style={{flex:1, marginTop:'10%'}}>
+
+        {sorted.length < 1 && <Empyty />}
+          </View>
+        </ScrollView>
+      
+        </>
+      )}
       <Navigation />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
     width: '100%',
     alignItems: 'center',
     paddingBottom: 60,
+    backgroundColor: '#fff',
   },
 });
