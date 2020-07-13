5b982281f53ab1fac83476ba2a968b76a11dc3ff
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-07-13 15:26:10 +0100

    chore: sort completed todos

--- screens/CompletedTodos.tsx
+++ screens/CompletedTodos.tsx
@@ -2,47 +2,53 @@ import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
 //components
 import TodoItem from '../components/TodoItem';
-import Navigation from '../components/Navigation'
+import Navigation from '../components/Navigation';
+import Empty from '../components/EmtpyState';
 
 //redux
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchTodos } from '../store/actions/todos';
-
+import EmtpyState from '../components/EmtpyState';
 
 interface ITodo {
-    title: string;
-    description: string;
-    dueDate: string;
-    isCompleted: boolean;
-  }
+  title: string;
+  description: string;
+  dueDate: string;
+  isCompleted: boolean;
+}
 
 export default function AllTodos() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-      dispatch(fetchTodos());
-    }, []);
-    const { todos } = useSelector((state) => state?.todos);
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, []);
+  const { todos } = useSelector((state) => state?.todos);
   
-    return (
-      <View style={{ flex: 1 }}>
-        <ScrollView contentContainerStyle={styles.container}>
-          {todos?.map((todo: ITodo, index: number) => (
-            <React.Fragment key={index}>
-              {todo.isCompleted && <TodoItem todo={todo}  />}
-            </React.Fragment>
-          ))}
-        </ScrollView>
-        <Navigation />
-      </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      width: '100%',
-      alignItems: 'center',
-    },
+  const completedTodos = todos?.filter((todo: ITodo, index: number) => {
+    if (todo.isCompleted) {
+      return (
+        <React.Fragment key={index}>
+          {todo.isCompleted && <TodoItem todo={todo} />}
+        </React.Fragment>
+      );
+    }
   });
-  
+
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        {completedTodos.length > 0 ? completedTodos : <EmtpyState />}
+      </ScrollView>
+      <Navigation />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+  },
+});
