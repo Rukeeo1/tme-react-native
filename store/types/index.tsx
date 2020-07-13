b70ec58214b96e307267e978b5417bad805b4e6e
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-07-13 15:31:52 +0100

    chore: add corresponding types

--- store/types/index.tsx
+++ store/types/index.tsx
@@ -1,5 +1,10 @@
-export const FETCH_TODOS = 'FETCH_TODOS'
-export const FILTER_TODOS = 'FILTER_TODOS '
-export const UPDATE_TODO = 'UPDATE_TODO'
-export const DELETE_TODO = 'DELETE_TODO' 
+export const FETCH_TODOS = 'FETCH_TODOS';
+export const FILTER_TODOS = 'FILTER_TODOS ';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
 
+//users
+export const ADD_USER = 'ADD_USER';
+
+//Loading
+export const LOADING = 'LOADING';
